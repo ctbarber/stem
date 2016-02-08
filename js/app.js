57134ab827d2204a50086144958399aad3fc760e
Todd Barber <ctbarber@gmail.com>
2016-02-08 11:25:15 -0600

    changing colors of check boxes

--- js/app.js
+++ js/app.js
@@ -1,1 +1,14 @@
 $(document).foundation();
+
+$(document).ready(function() {
+    $( ".color-box" ).blur(function() {
+        var idOfColor = $(this).attr('id').split("-");
+        var newRed = $('#red-'+idOfColor[1]).val();
+        var newGreen = $('#green-'+idOfColor[1]).val();
+        var newBlue = $('#blue-'+idOfColor[1]).val();
+        if ((newRed).trim().length == 0) {newRed = 0;}
+        if ((newGreen).trim().length == 0) {newGreen = 0;}
+        if ((newBlue).trim().length == 0) {newBlue = 0;}
+        $('.check-color-'+idOfColor[1]).css("color","rgb("+newRed+","+newGreen+","+newBlue+")");
+    });
+});
